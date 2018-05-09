d05152c4a0b412b066d569e8c5550bbc21664f72
Mandeep Sarangal <manusarangal@gmail.com>
2018-05-09 15:32:30 -0400

    Counting App Prototype Ready

---
+++ Prototype/CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  Prototype
+//
+//  Created by Mandeep Sarangal on 2018-05-09.
+//  Copyright © 2018 Mandeep Sarangal. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var teddyCount: UILabel!
+}
--- Prototype/ViewController.swift
+++ Prototype/ViewController.swift
@@ -8,11 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    @IBOutlet weak var labelCount: UILabel!
+    
+    var count = 0
+    var teddyCollection = [String]()
+    //var teddyCollection = ["1", "2", "3", "4", "5", "6", "7", "8"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        collectionView.dataSource = self
+        collectionView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +30,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func teddyTapped(_ sender: Any) {
+        count = count + 1
+        teddyCollection.append("\(count)")
+        labelCount.text = "\(count)"
+        collectionView.reloadData()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return teddyCollection.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! CollectionViewCell
+        cell.teddyCount.text = teddyCollection[indexPath.item]
+        return cell
+    }
+    
 }
 
